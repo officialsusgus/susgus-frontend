d20b9d3cedd1326bcb9214383ffef02a3472a9e5
Exotical <ThatMogus@outlook.com>
2023-01-27 23:28:30 +0100

    Clean up a little bit
    This script will be deeply optimized soon.

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,9 @@ sv_SE = {
     "heading": "Se skolmaten.",
     "description": "Med Sus Gus kan du se skolmaten som serveras på din skola med advancerad AI-teknik som visualiserar skolmaten.",
     "main_button": "Se skolmaten",
-    "secondary_button": "Annat"
+    "secondary_button": "Annat",
+    "food_loading_title": "Hämtar skolmat...",
+    "food_loading_subtitle": "Var god vänta, detta kan ta en stund."
 }
 en_UK = {
     "title": "English (United Kingdom)",
@@ -26,40 +28,48 @@ en_UK = {
     "heading": "See the school food.",
     "description": "With Sus Gus, you can see the food served in your school with advanced AI-technology that visualizes the school food.",
     "main_button": "See food",
-    "secondary_button": "Other features"
+    "secondary_button": "Other features",
+    "food_loading_title": "Fetching food...",
+    "food_loading_subtitle": "Please wait, this could take a moment."
 }
 function loadLanguage() {
     if(localStorage.getItem("language") == null) {
         localStorage.setItem("language", "sv_SE");
     };
     if(localStorage.getItem("language") == "sv_SE") {
-        $("#home").text(sv_SE["home"])
-        $("#food").text(sv_SE["food"])
-        $("#clicker").text(sv_SE["clicker"])
-        $("#countdowns").text(sv_SE["countdowns"])
-        $("#about").text(sv_SE["about"])
-        $("#title").text(sv_SE["heading"])
-        $("#description").text(sv_SE["description"])
-        $("#main_button").text(sv_SE["main_button"])
-        $("#secondary_button").text(sv_SE["secondary_button"])
+        $("#home").text(sv_SE["home"]);
+        $("#food").text(sv_SE["food"]);
+        $("#clicker").text(sv_SE["clicker"]);
+        $("#countdowns").text(sv_SE["countdowns"]);
+        $("#about").text(sv_SE["about"]);
+        $("#code").text(sv_SE["code"]);
+        $("#title").text(sv_SE["heading"]);
+        $("#description").text(sv_SE["description"]);
+        $("#main_button").text(sv_SE["main_button"]);
+        $("#secondary_button").text(sv_SE["secondary_button"]);
         $("#lang_text").text(sv_SE["title"]);
         $("#lang_image").attr("src", sv_SE["flag"]);
         $("#langmenu_text").text(en_UK["title"]);
         $("#langmenu_image").attr("src", en_UK["flag"]);
+        $("#main").text(en_UK["food_loading_title"]);
+        $("#alternative").text(en_UK["food_loading_subtitle"]);
     } else {
-        $("#home").text(en_UK["home"])
-        $("#food").text(en_UK["food"])
-        $("#clicker").text(en_UK["clicker"])
-        $("#countdowns").text(en_UK["countdowns"])
-        $("#about").text(en_UK["about"])
-        $("#title").text(en_UK["heading"])
-        $("#main_button").text(en_UK["main_button"])
-        $("#secondary_button").text(en_UK["secondary_button"])
-        $("#description").text(en_UK["description"])
+        $("#home").text(en_UK["home"]);
+        $("#food").text(en_UK["food"]);
+        $("#clicker").text(en_UK["clicker"]);
+        $("#countdowns").text(en_UK["countdowns"]);
+        $("#about").text(en_UK["about"]);
+        $("#code").text(en_UK["code"]);
+        $("#title").text(en_UK["heading"]);
+        $("#main_button").text(en_UK["main_button"]);
+        $("#secondary_button").text(en_UK["secondary_button"]);
+        $("#description").text(en_UK["description"]);
         $("#lang_text").text(en_UK["title"]);
         $("#lang_image").attr("src", en_UK["flag"]);
         $("#langmenu_text").text(sv_SE["title"]);
         $("#langmenu_image").attr("src", sv_SE["flag"]);
+        $("#main").text(sv_SE["food_loading_title"]);
+        $("#alternative").text(sv_SE["food_loading_subtitle"]);
     };
 };
 function changeLanguage() {
@@ -73,8 +83,7 @@ function changeLanguage() {
 };
 async function reloadSchoolFood() {
     $("#food_image").attr("src", "assets/img/loading.gif");
-    $("#main").text("Hämtar skolmat...");
-    $("#alternative").text("Var god vänta, detta kan ta en stund.");
+    loadLanguage();
     $.getJSON("https://susapi.emilioaliustic.repl.co/Misc/schoolFood", function(data) {
         if (data.alternative == "Ledigt") {
           $("#main").text(data.food);
@@ -84,9 +93,9 @@ async function reloadSchoolFood() {
           $("#alternative").text(data.alternative);
         };
     });
-    $.get("https://susapi.emilioaliustic.repl.co/Misc/schoolFoodImage?random=" + Math.random());
+    $.get("https://susapi.emilioaliustic.repl.co/Misc/schoolFoodImage");
     await new Promise(r => setTimeout(r, 2000));
-    $("#food_image").attr("src", "https://susapi.emilioaliustic.repl.co/Misc/schoolFoodImage?random=" + Math.random());
+    $("#food_image").attr("src", "https://susapi.emilioaliustic.repl.co/Misc/schoolFoodImage");
 };
 function executePageFunctions(page) {
     loadLanguage();
